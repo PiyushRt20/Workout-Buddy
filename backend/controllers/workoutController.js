25239ded27702d6afc4f8f1376ea78b26ad2483c
PiyushRt20 <piyushrawat4000@gmail.com>
2026-06-21 22:59:09 +0530

    Completed Workout Buddy project

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -28,6 +28,19 @@ exports.getWorkout = async(req, res)=>{
 //create a new workout
 exports.createWorkout = async (req, res) => {
     const { title, load, reps } = req.body;
+
+    let emptyFields = [];
+    if(!title){
+        emptyFields.push('title')
+    }else if(!load){
+        emptyFields.push('load')
+    }else if(!reps){
+        emptyFields.push('reps')
+    }
+    if(emptyFields.length > 0){
+        return res.status(400).json({error : "Please fill all the fields", emptyFields});
+    }
+
     try {
         const workout = await Workout.create({title, load, reps});
         res.status(201).json(workout);
