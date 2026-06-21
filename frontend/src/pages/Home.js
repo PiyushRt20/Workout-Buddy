d2e5efcdb77bbf28c111634924b2e9a6cf94bbfa
PiyushRt20 <piyushrawat4000@gmail.com>
2026-06-21 23:02:36 +0530

    Fix frontend submodule

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,40 @@
+// import React, { useEffect, useState } from 'react'
+import React, { useEffect} from 'react'
+
+//components
+import WorkoutDetails from '../components/WorkoutDetails';
+import WorkoutForm from '../components/WorkoutForm';
+import { useWorkoutsContext } from '../Hooks/useWorkoutsContext';
+
+
+const Home = () => {
+    // const [workouts, setWorkout] = useState(null)
+    const {workouts, dispatch} = useWorkoutsContext();
+    useEffect(()=>{
+        const fetchWorkouts = async ()=>{
+            const response = await fetch('/api/workouts/')
+            const json = await response.json()
+
+            if(response.ok){
+                // setWorkout(json)
+                dispatch({type: 'SET_WORKOUTS', payload : json})
+            }
+        }
+        fetchWorkouts()
+    }, [dispatch])
+return (
+    <div className="home">
+      <div className="workouts">
+        {
+          workouts && workouts.map((workout) => (
+            // <p key={workout._id}>{workout.title}</p>
+            <WorkoutDetails key = {workout._id} workout = {workout}/>
+          ))
+        }
+      </div>
+      <WorkoutForm/>
+    </div>
+  );
+};
+
+export default Home;
